280d4aa15baa313c21a1aae0ccefbdca30dbc5ae
Antoine <antoinedujardin42@gmail.com>
2020-09-22 14:25:32 +0200

    add TD2

---
+++ TD2/CODE/plugin_TP2_7.cpp
@@ -0,0 +1,84 @@
+#include <gcc-plugin.h>                                                          
+#include <plugin-version.h>                                                      
+#include <tree.h>                                                                
+#include <basic-block.h>                                                         
+#include <gimple.h>                                                              
+#include <tree-pass.h>                                                           
+#include <context.h>                                                             
+#include <function.h>                                                            
+#include <gimple-iterator.h>                                                     
+
+
+/******************************/
+/****   TD2 - QUESTION 7   ****/
+/******************************/
+
+/* Build a filename (as a string) based on function name */
+	static char * 
+cfgviz_generate_filename( function * fun, const char * suffix )
+{
+	char * target_filename ; 
+
+	target_filename = (char *)xmalloc( 2048 * sizeof( char ) ) ;
+
+	snprintf( target_filename, 1024, "%s_%s_%d_%s.dot", 
+			current_function_name(),
+			LOCATION_FILE( fun->function_start_locus ),
+			LOCATION_LINE( fun->function_start_locus ),
+			suffix ) ;
+
+	return target_filename ;
+}
+
+/* Dump the graphviz representation of function 'fun' in file 'out' */
+	static void 
+cfgviz_internal_dump( function * fun, FILE * out ) 
+{
+
+	// Print the header line and open the main graph
+	fprintf(out, "Digraph G{\n");
+
+
+	/*****************************/
+	/***** COMPLETE HERE ********/	
+	/*****************************/
+
+	// Close the main graph
+	fprintf(out, "}\n");
+}
+
+	void 
+cfgviz_dump( function * fun, const char * suffix )
+{
+	char * target_filename ; 
+	FILE * out ;
+
+	target_filename = cfgviz_generate_filename( fun, suffix ) ;
+
+	printf( "[GRAPHVIZ] Generating CFG of function %s in file <%s>\n",
+			current_function_name(), target_filename ) ;
+
+	out = fopen( target_filename, "w" ) ;
+
+	cfgviz_internal_dump( fun, out ) ;
+
+	fclose( out ) ;
+	free( target_filename ) ;
+}
+
+/******************************/
+/**   TD2 - FIN QUESTION 7   **/
+/******************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
