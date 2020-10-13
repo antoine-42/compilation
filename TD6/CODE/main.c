abdc76b7774a79d017d65a9681fbcd139a3cf5d2
Antoine <antoinedujardin42@gmail.com>
2020-10-13 10:15:58 +0200

    add td6

---
+++ TD6/CODE/main.c
@@ -0,0 +1,15 @@
+/**
+ * \file main.c
+ * \brief TEST PLUGIN
+*/
+#include <stdio.h>
+#include "mock.h"
+
+int main (int argc, char **argv) {
+
+	printf("begining main\n");
+	mock1();
+	printf("ending main\n");
+
+	return 0;
+}
